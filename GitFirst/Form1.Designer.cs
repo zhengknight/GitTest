a8c13e02f9eeeaea0bb714dd4a0d95e48b79d9c5
zheng <zhengknight@qq.com>
2015-10-15 13:44:41 +0800

    添加按钮再次

--- GitFirst/Form1.Designer.cs
+++ GitFirst/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnCeshi = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCeshi
@@ -41,11 +42,22 @@
             this.btnCeshi.UseVisualStyleBackColor = true;
             this.btnCeshi.Click += new System.EventHandler(this.btnCeshi_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "再次";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCeshi);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnCeshi;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GitFirst/Form1.cs
+++ GitFirst/Form1.cs
@@ -20,5 +20,10 @@ namespace GitFirst
         {
             MessageBox.Show("haha aa");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("再弄个按钮！");
+        }
     }
 }
